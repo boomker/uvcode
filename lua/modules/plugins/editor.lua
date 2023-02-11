c47fcf87edf00bb760b55c75d952565296e1b805
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-11 00:29:41 -0800

    chore(treesitter): only TSUpdate when UI is available
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -69,8 +69,12 @@ editor["romainl/vim-cool"] = {
 ----------------------------------------------------------------------
 editor["nvim-treesitter/nvim-treesitter"] = {
 	lazy = true,
-	build = ":TSUpdate",
-	event = { "CursorHold", "CursorHoldI" },
+	build = function()
+		if #vim.api.nvim_list_uis() ~= 0 then
+			vim.cmd("TSUpdate")
+		end
+	end,
+	event = "BufReadPost",
 	config = require("editor.treesitter"),
 	dependencies = {
 		{ "nvim-treesitter/nvim-treesitter-textobjects" },
