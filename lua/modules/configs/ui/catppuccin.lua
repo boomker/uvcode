b65596ef7a49051f0e03c3eb709644ddd1abbe9a
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-04-30 23:02:06 +0800

    fix(catppuccin): resolve undefined symbols
    (cherry picked from commit a78656475ae21505c2a672c552e58272d56fb755)

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -1,4 +1,6 @@
 return function()
+	local transparent_background = require("core.settings").transparent_background
+
 	require("catppuccin").setup({
 		flavour = "frappe", -- Can be one of: latte, frappe, macchiato, mocha
 		background = { light = "latte", dark = "frappe" },
@@ -9,7 +11,7 @@ return function()
 			shade = "dark",
 			percentage = 0.15,
 		},
-		transparent_background = require("core.settings").transparent_background,
+		transparent_background = transparent_background,
 		show_end_of_buffer = false, -- show the '~' characters after the end of buffers
 		term_colors = true,
 		compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
