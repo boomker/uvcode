af751f2b808c2cce67b6236a418433d4feb60984
ayamir <lgt986452565@gmail.com>
2023-07-10 13:14:38 +0800

    chore(plugins): update plugins with maintained fork. (#848)
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
    Co-authored-by: Charles Chiu <charleschiu@hey.com>
    (cherry picked from commit b260a4b8d30c137d3a7c1165b653aca5f0dea713)

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -102,7 +102,8 @@ editor["echasnovski/mini.bufremove"] = {
 
 editor["smoka7/hop.nvim"] = {
 	lazy = true,
-	event = "VeryLazy",
+	version = "*",
+	event = { "CursorHold", "CursorHoldI" },
 	config = require("editor.hop"),
 }
 
@@ -138,14 +139,17 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 			vim.api.nvim_command("TSUpdate")
 		end
 	end,
-	event = { "CursorHold", "CursorHoldI" },
+	event = "BufReadPre",
 	config = require("editor.treesitter"),
 	dependencies = {
 		{ "nvim-treesitter/nvim-treesitter-textobjects" },
-		{ "HiPhish/rainbow-delimiters.nvim" },
 		{ "JoosepAlviste/nvim-ts-context-commentstring" },
 		{ "mfussenegger/nvim-treehopper" },
 		{ "andymass/vim-matchup" },
+		{
+			"hiphish/rainbow-delimiters.nvim",
+			config = require("editor.rainbow_delims"),
+		},
 		{
 			"nvim-treesitter/nvim-treesitter-context",
 			config = require("editor.ts-context"),
