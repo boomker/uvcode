18d7d7b0d3122b48acb98037f86b403115622277
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-17 19:43:04 +0800

    perf!: consolidated updates to address performance issues (#868)
    * perf!: consolidated updates to address performance issues
    
    This fixes #866 and closes #867.
    
    * fixup! perf!: consolidated updates to address performance issues
    
    * revert: `nvim-bufdel` shall always be available (API calls)
    
    * feat(rainbow_delims): line detections for c/cpp
    
    ---------
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
    Co-authored-by: Layton <caolei6767@google.com>
    (cherry picked from commit 1165861a38680c979a0c07690fbaa9e17a66b3c5)

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -37,7 +37,7 @@ editor["LunarVim/bigfile.nvim"] = {
 
 --[[ editor["rhysd/clever-f.vim"] = {
 	lazy = true,
-	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	event = { "CursorHold", "CursorHoldI" },
 	config = require("editor.cleverf"),
 } ]]
 
@@ -139,7 +139,7 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 			vim.api.nvim_command("TSUpdate")
 		end
 	end,
-	event = "BufReadPre",
+	event = "BufReadPost",
 	config = require("editor.treesitter"),
 	dependencies = {
 		{ "nvim-treesitter/nvim-treesitter-textobjects" },
