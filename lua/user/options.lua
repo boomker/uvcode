28a733a9e66b67f0f7b443826bf0f4632324269c
Shingo <boomker@users.noreply.github.com>
2025-05-10 11:14:21 +0800

    update config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,6 +8,6 @@ local options = {
 	-- laststatus = 2,
 }
 
-vim.g.python_host_prog = "~/.pyvenvs/neovim/.venv/bin/python"
-vim.g.python3_host_prog = "~/.pyvenvs/neovim/.venv/bin/python3"
+vim.g.python_host_prog = "/opt/homebrew/bin/python"
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
 return options
