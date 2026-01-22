a0bdf751686ed89fedd4ac7205ec2f1ee4bd1aec
Shingo <boomker@users.noreply.github.com>
2026-01-22 13:29:08 +0800

    update config

--- lua/user/configs/lsp-servers/ruff.lua
+++ lua/user/configs/lsp-servers/ruff.lua
@@ -1,12 +1,41 @@
-local util = require("lspconfig.util")
+--[[ local util = require("lspconfig.util")
 
 return {
 	cmd = { "ruff", "server" },
 	filetypes = { "python" },
 	root_dir = function(fname)
 		return util.root_pattern("pyproject.toml", "ruff.toml", ".ruff.toml")(fname)
-			or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+		-- or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
 	end,
 	single_file_support = true,
 	settings = {},
+} ]]
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ruff.lua
+return {
+	cmd = { "ruff", "server" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+
+	-- the following are added by nvimdots
+	settings = {
+		init_options = {
+			settings = {
+				lint = {
+					select = {
+						-- enable: pycodestyle
+						"E",
+						-- enable: pyflakes
+						"F",
+					},
+					extendSelect = {
+						-- enable: isort
+						"I",
+					},
+					-- the same line length as black
+					lineLength = 88,
+				},
+				configurationPreference = "filesystemFirst",
+			},
+		},
+	},
 }
